dcfbeea275d6679c45bba818adf421e4bd051567
Kristijan Rupić <krupic.2.0@gmail.com>
2018-06-23 19:33:27 +0200

    replace hand-rolled prompt with readline
    break down main loop into separate functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate lambda_rust;
+extern crate rustyline;
 
 use lambda_rust::{
     lambda::*,
@@ -6,58 +7,80 @@ use lambda_rust::{
     parser::parse,
 };
 
-use std::io::{self, Write};
+use rustyline::error::ReadlineError;
+
 use std::collections::HashSet;
+use std::process;
 
 fn main() {
+    let mut editor = rustyline::Editor::<()>::new();
+
     loop {
-        print!("> ");
-        io::stdout().flush().expect("Failed to flush stdout.");
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read input.");
-
-        if ":quit".starts_with(input.trim()) && input.trim().len() > 1 {
-            break;
-        }
-
-        let tokens = Token::parse_all(&input);
-        if let Err(ref e) = tokens {
-            println!("{}", e.0);
-            continue;
-        }
-
-        let tokens = tokens.unwrap();
-        let term = parse(&tokens);
-        match term {
-            Err(ref e) => {
-                println!("{}", e);
+        let input = match editor.readline("> ") {
+            Ok(line) => {
+                editor.add_history_entry(&line);
+                line
+            }
+            Err(ReadlineError::Interrupted) => {
+                exit();
+            }
+            e @ Err(_) => {
+                println!("Error: {:?}", e);
                 continue;
             }
-            Ok(ref t) => println!(" : {}", t),
+        };
+
+        if ":quit".starts_with(input.trim()) && input.trim().len() > 1 {
+            exit();
         }
 
+        interpret(input);
+    }
+}
 
-        let mut term = term.unwrap();
-        let mut seen_terms = HashSet::new();
-        loop {
-            let reduct = term.reduce(Strategy::NormalOrder);
-            print!("β: ");
-            match reduct {
-                EvalResult::NormalForm(r) => {
-                    println!("{} [normal]", r);
+fn interpret<S: AsRef<str>>(input: S) {
+    let tokens = Token::parse_all(input.as_ref());
+    if let Err(ref e) = tokens {
+        println!("{}", e.0);
+        return;
+    }
+
+    let tokens = tokens.unwrap();
+    let term = parse(&tokens);
+    match term {
+        Err(ref e) => {
+            println!("{}", e);
+            return;
+        }
+        Ok(ref t) => println!(" : {}", t),
+    }
+
+
+    let mut term = term.unwrap();
+    let mut seen_terms = HashSet::new();
+    loop {
+        let reduct = term.reduce(Strategy::NormalOrder);
+        print!("β: ");
+        match reduct {
+            EvalResult::NormalForm(r) => {
+                println!("{} [normal]", r);
+                break;
+            }
+            EvalResult::PossiblyReducible(r) => {
+                if !seen_terms.contains(&r) {
+                    println!("{}", r);
+                    term = r;
+                    seen_terms.insert(term.clone());
+                } else {
+                    println!("[non-terminating]");
                     break;
-                }
-                EvalResult::PossiblyReducible(r) => {
-                    if !seen_terms.contains(&r) {
-                        println!("{}", r);
-                        term = r;
-                        seen_terms.insert(term.clone());
-                    } else {
-                        println!("[non-terminating]");
-                        break;
-                    }
                 }
             }
         }
     }
+}
+
+fn exit() -> ! {
+    println!("Exiting ...");
+    process::exit(0);
 }
